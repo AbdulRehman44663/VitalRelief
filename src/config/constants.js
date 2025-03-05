b0cf1cbff5c79699224ec4516da63f5be7e7f0fe
AbdulRehman44663 <abdulrehman44661@gmail.com>
2025-03-05 15:15:54 +0500

    Code push

---
+++ src/config/constants.js
@@ -0,0 +1,10 @@
+export const API_URL =  'http://localhost:8000/api/user';
+export const BLOG_IMAGES_PATH =  'http://localhost:8000/assets/uploads/blog-images';
+export const LISTING_IMAGES_PATH =  'http://localhost:8000/assets/uploads/listing-images';
+export const IMAGES_PATH =  'http://localhost:8000/assets/uploads/images';
+export const SITE_URL = 'http://staging.vitalrelief.com';
+export const API_DOMAIN = 'http://api.vitalrelief.com';
+export const GET_DIRECTION_URL = 'https://maps.google.com/?q=';
+
+export const BUSINESS_EMAIL ='sb-3rrni36550304@business.example.com';
+export const PAYPAL_MODE ='sandbox';
